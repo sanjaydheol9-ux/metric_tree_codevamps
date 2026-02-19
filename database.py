3735019b08d3a9940fa2c1eb434a1fd45b3f9857
sanjaydheol9-ux <sanjaydheol9@gmail.com>
2026-02-20 04:30:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = postgresql://postgres:sanjaydheol2006@db.ykgyknhrjvkktpeossks.supabase.co:5432/postgres
-
+DATABASE_URL =postgresql://postgres:sanjaydheol9906@db.ykgyknhrjvkktpeossks.supabase.co:5432/postgres
 engine = create_engine(
     DATABASE_URL,
     connect_args={"sslmode": "require"}
