6f01dbcd174d3a57278fd222d93508ce09b2896a
sanjaydheol9-ux <sanjaydheol9@gmail.com>
2026-02-20 04:00:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,10 @@ import os
 
 DATABASE_URL = "postgresql://postgres:kkgGL*kv&W_yQL7@db.ykgyknhrjvkktpeossks.supabase.co:5432/postgres"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
