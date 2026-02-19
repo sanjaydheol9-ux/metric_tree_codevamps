f4928146baba6f00e9896a4631a47f1483d42d6d
sanjaydheol9-ux <sanjaydheol9@gmail.com>
2026-02-20 03:28:05 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class WeeklyMetrics(Base):
     __tablename__ = "weekly_metrics"
 
     id = Column(Integer, primary_key=True, index=True)
-    week = Column(Integer, index=True)
+    week = Column(Integer, nullable=False)
     delivery_score = Column(Float)
     accuracy_score = Column(Float)
     dispatch_score = Column(Float)
