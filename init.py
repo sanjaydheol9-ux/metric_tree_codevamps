200d0e0354bbf75c2bedb8090abb36f29287025c
sanjaydheol9-ux <sanjaydheol9@gmail.com>
2026-02-19 18:40:48 +0530

    Update import statements to use new module names

--- init.py
+++ init.py
@@ -1,3 +1,3 @@
-from ai.ai_service import generate_week_insights
-from ai.llm_client import call_llm
-from ai.context_builder import build_context
+from Aiservice import generate_week_insights
+from Llmclient import call_llm
+from Contextbuilder import build_context
