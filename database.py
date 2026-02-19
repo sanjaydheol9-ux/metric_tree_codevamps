16c5ecac99d7aa2d0b23a5e10365725b0d4323d7
sanjaydheol9-ux <sanjaydheol9@gmail.com>
2026-02-20 04:15:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = postgresql://postgres:[YOUR-PASSWORD]@db.ykgyknhrjvkktpeossks.supabase.co:5432/postgres
+DATABASE_URL = postgresql://postgres:kkgGL*kv&W_yQL7@db.ykgyknhrjvkktpeossks.supabase.co:5432/postgres
 
 engine = create_engine(
     DATABASE_URL,
